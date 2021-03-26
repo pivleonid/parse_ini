6cd240980a5b860e8053cece907e16428d056b3d
Nicki <andreich1922@yandex.ru>
2021-03-26 20:35:47 +0300

    Add file CMap_test.h

--- CMap_test.h
+++ CMap_test.h
@@ -1,4 +1,36 @@
-#ifndef CMAP_TEST_H
-#define CMAP_TEST_H
+#ifndef MAP_TEST_H
+#define MAP_TEST_H
+#include "CMap.h"
 
-#endif // CMAP_TEST_H
+#include <gtest/include/gtest/gtest.h>
+using namespace std;
+
+TEST(test037, test_CMap)
+{
+    Map a;
+    CString b("Zero");
+    CString c("WALKYRIE");
+    CString d("New");
+    CString e("X-men");
+    CString f("developer");
+    CString g("match");
+    CString h("view");
+    CString i("hero");
+    CString j("Zero");
+    CString k("hero");
+    CString l("big_boss");
+    a.add(b);
+    a.add(c);
+    a.add(d);
+    a.add(e);
+    a.add(f);
+    a.add(g);
+    a.add(h);
+    a.add(i);
+    a.add(j);
+    a.add(k);
+    a.add(l);
+    bool res = a.search("Zero");
+    ASSERT_EQ(1, res);
+}
+#endif // MAP_TEST_H
