e41cedfa3ffedf773f73f54b72d01996db97fa4a
Nicki <andreich1922@yandex.ru>
2021-03-26 20:36:50 +0300

    Add file CMap.h

--- CMap.h
+++ CMap.h
@@ -1,4 +1,29 @@
 #ifndef MAP_H
 #define MAP_H
-
+#include "CString.h"
+#include "string"
+#include "cstring"
+#include <iostream>
+using namespace std;
+struct Node
+{
+    const char  *str;
+    Node        *left;
+    Node        *right;
+};
+class Map
+{
+private:
+    Node *root;
+    bool find;
+    Node *key;
+public:
+    Map();
+    ~Map();
+    void add(CString &str);
+    Node *add(const char *new_str, Node *tree);
+    void walk(const char *target, Node *tree);
+    bool search(const char *target);
+    void delete_elem(Node *tree);
+};
 #endif // MAP_H
