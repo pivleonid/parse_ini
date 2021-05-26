86d0c3dd3ebfeecac48a29ddbd674668d515a9af
Nicki <andreich1922@yandex.ru>
2021-05-26 18:39:02 +0300

    Modified CIni

--- CIni.cpp
+++ CIni.cpp
@@ -36,7 +36,7 @@ void CIni::analyze_file()
             temp_container.at(i).pop_front();
             temp_container.at(i).pop_back();
             temp_container.at(i).pop_back();
-            temp_content_of_section.name_section = temp_container.at(i);
+            temp_content_of_section.m_name_section = temp_container.at(i);
             count++;
         }
         else if(temp_container.at(i).front() == ';')
@@ -48,7 +48,7 @@ void CIni::analyze_file()
                 unsigned start = temp_container.at(i).find(' ');
                 unsigned end = temp_container.at(i).find('\n');
                 temp_comment.erase(start, end);
-                temp_content_of_section.name_comment.push_back(temp_comment);
+                temp_content_of_section.m_name_comment.push_back(temp_comment);
                 temp_comment.clear();
 
                 end = temp_container.at(i).find(' ');
@@ -59,7 +59,7 @@ void CIni::analyze_file()
             if(!temp_container.at(i).find(' '))
             {
                 temp_container.at(i).pop_back();
-                temp_content_of_section.name_comment.push_back(temp_container.at(i));
+                temp_content_of_section.m_name_comment.push_back(temp_container.at(i));
             }
         }
         else if(temp_container.at(i).empty())
@@ -103,12 +103,10 @@ void CIni::analyze_file()
                  temp_container.at(i).pop_back();
                  temp_vector_value.push_back(temp_container.at(i));
              }
-             temp_content_of_section.key_value.add_pair(temp_key, temp_vector_value);
+             temp_content_of_section.m_key_value.add_pair(temp_key, temp_vector_value);
              temp_key.clear();
              temp_vector_value.clear();
         }
-
-
     }
     if(count == 1)
     {
@@ -135,8 +133,15 @@ void CIni::write_file(const char *file_path)
     {
         for(unsigned j = 0; j < temp_container.at(i).size(); j++)
         {
-            ch = temp_container.at(i).at(j);
-            fout << ch;
+            if(temp_container.at(i).at(j) == '\0')
+            {
+                continue;
+            }
+            else
+            {
+                ch = temp_container.at(i).at(j);
+                fout << ch;
+            }
         }
     }
 }
@@ -147,20 +152,20 @@ void CIni::write_file_inner()
     for(unsigned i = 0; i < data.size(); i++)
     {
         CString temp;
-        temp = data.at(i).name_section;
+        temp = data.at(i).m_name_section;
         temp.push_front('[');
         temp.push_back(']');
         temp.push_back('\n');
         temp_container.push_back(temp);
         k++;
-        if(!data.at(i).name_comment.is_empty())
+        if(!data.at(i).m_name_comment.is_empty())
         {
             temp.clear();
             temp_container.push_back(temp);
             temp_container.at(k).push_back(';');
-            for(unsigned n = 0; n < data.at(i).name_comment.size(); n++)
+            for(unsigned n = 0; n < data.at(i).m_name_comment.size(); n++)
             {
-                temp_container.at(k).push_back(data.at(i).name_comment.at(n));
+                temp_container.at(k).push_back(data.at(i).m_name_comment.at(n));
                 temp_container.at(k).push_back(' ');
             }
             temp_container.at(k).pop_back();
@@ -168,21 +173,21 @@ void CIni::write_file_inner()
             k++;
         }
         temp.clear();
-        //unsigned size = data.at(i).key_value.size();
+        //unsigned size = data.at(i).m_key_value.size();
         unsigned j = 1;
         CString node;
         unsigned count = 0;
         node = j;
-        while(count != data.at(i).key_value.size())
+        while(count != data.at(i).m_key_value.size())
         {
-            if(data.at(i).key_value.search(node))
+            if(data.at(i).m_key_value.search(node))
             {
                 temp_container.push_back(temp);
                 temp_container.at(k).push_back(node);
                 temp_container.at(k).push_back(" = ");
-                for(unsigned l = 0; l < data.at(i).key_value.getValue(node).size(); l++)
+                for(unsigned l = 0; l < data.at(i).m_key_value.getValue(node).size(); l++)
                 {
-                    temp_container.at(k).push_back(data.at(i).key_value.getValue(node).at(l).data());
+                    temp_container.at(k).push_back(data.at(i).m_key_value.getValue(node).at(l).data());
                     temp_container.at(k).push_back(", ");
                 }
                 temp_container.at(k).pop_back();
@@ -198,13 +203,13 @@ void CIni::write_file_inner()
     }
 }
 
-void CIni::change_name_section(CString &old_name, CString &new_name)
+void CIni::change_m_name_section(CString &old_name, CString &new_name)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(old_name == data.at(i).name_section)
+        if(old_name == data.at(i).m_name_section)
         {
-            data.at(i).name_section = new_name;
+            data.at(i).m_name_section = new_name;
         }
     }
 }
@@ -214,26 +219,26 @@ void CIni::show_all()
     for(unsigned i = 0; i < data.size(); i++)
     {
         CString node;
-        cout << data.at(i).name_section.data() << endl;
-        if(!data.at(i).name_comment.is_empty())
+        cout << data.at(i).m_name_section.data() << endl;
+        if(!data.at(i).m_name_comment.is_empty())
         {
-            for(unsigned j = 0; j < data.at(i).name_comment.size(); j++)
+            for(unsigned j = 0; j < data.at(i).m_name_comment.size(); j++)
             {
-                cout << data.at(i).name_comment.at(j).data() << ' ';
+                cout << data.at(i).m_name_comment.at(j).data() << ' ';
             }
             cout << endl;
         }
         unsigned k = 1;
         unsigned count = 0;
         node = k;
-        while (count != data.at(i).key_value.size())
+        while (count != data.at(i).m_key_value.size())
         {
-            if(data.at(i).key_value.search(node))
+            if(data.at(i).m_key_value.search(node))
             {
                 cout << k << " = ";
-                for(unsigned l = 0; l < data.at(i).key_value.getValue(node).size(); l++)
+                for(unsigned l = 0; l < data.at(i).m_key_value.getValue(node).size(); l++)
                 {
-                    cout << data.at(i).key_value.getValue(node).at(l).data() << ' ';
+                    cout << data.at(i).m_key_value.getValue(node).at(l).data() << ' ';
                 }
                 cout << endl;
                 count++;
@@ -245,93 +250,93 @@ void CIni::show_all()
     }
 }
 
-void CIni::delete_value(CString &name_section, CString &key)
+void CIni::delete_value(CString &m_name_section, CString &key)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
-            if(data.at(i).key_value.search(key))
+            if(data.at(i).m_key_value.search(key))
             {
-                data.at(i).key_value.delete_key(key);
+                data.at(i).m_key_value.delete_key(key);
             }
         }
     }
 }
 
-void CIni::delete_value(CString &name_section, CString &key, unsigned n)
+void CIni::delete_value(CString &m_name_section, CString &key, unsigned n)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
-            if(data.at(i).key_value.search(key))
+            if(data.at(i).m_key_value.search(key))
             {
                 CVector<CString> temp;
-                temp = data.at(i).key_value.getValue(key);
-                data.at(i).key_value.delete_key(key);
+                temp = data.at(i).m_key_value.getValue(key);
+                data.at(i).m_key_value.delete_key(key);
 
                 temp.erase(n);
-                data.at(i).key_value.add_pair(key, temp);
+                data.at(i).m_key_value.add_pair(key, temp);
             }
         }
     }
 }
 
-void CIni::change_value(CString &name_section, CString &key, CVector<CString> &value)
+void CIni::change_value(CString &m_name_section, CString &key, CVector<CString> &value)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
-            if(data.at(i).key_value.search(key))
+            if(data.at(i).m_key_value.search(key))
             {
-                data.at(i).key_value.delete_key(key);
-                data.at(i).key_value.add_pair(key, value);
+                data.at(i).m_key_value.delete_key(key);
+                data.at(i).m_key_value.add_pair(key, value);
             }
         }
     }
 }
 
-void CIni::change_value(CString &name_section, CString &key, CString &value, unsigned n)
+void CIni::change_value(CString &m_name_section, CString &key, CString &value, unsigned n)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
             CVector<CString> temp;
-            temp = data.at(i).key_value.getValue(key);
-            data.at(i).key_value.delete_key(key);
+            temp = data.at(i).m_key_value.getValue(key);
+            data.at(i).m_key_value.delete_key(key);
 
             temp.at(n) = value;
-            data.at(i).key_value.add_pair(key, temp);
+            data.at(i).m_key_value.add_pair(key, temp);
         }
     }
 }
 
-void CIni::add_value(CString &name_section, CString &key, CVector<CString> &value)
+void CIni::add_value(CString &m_name_section, CString &key, CVector<CString> &value)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
-            data.at(i).key_value.add_pair(key, value);
+            data.at(i).m_key_value.add_pair(key, value);
         }
     }
 }
 
-void CIni::add_value(CString &name_section, CString &key, CString &value)
+void CIni::add_value(CString &m_name_section, CString &key, CString &value)
 {
     for(unsigned i = 0; i < data.size(); i++)
     {
-        if(name_section == data.at(i).name_section)
+        if(m_name_section == data.at(i).m_name_section)
         {
             CVector<CString> temp;
-            temp = data.at(i).key_value.getValue(key);
-            data.at(i).key_value.delete_key(key);
+            temp = data.at(i).m_key_value.getValue(key);
+            data.at(i).m_key_value.delete_key(key);
 
             temp.push_back(value);
-            data.at(i).key_value.add_pair(key, temp);
+            data.at(i).m_key_value.add_pair(key, temp);
         }
     }
 }
--- CIni.h
+++ CIni.h
@@ -12,22 +12,22 @@ class Content_of_section
 {
 public:
     /*!
-     * \brief name_section - название секции
+     * \brief m_name_section - название секции
      */
-    CString name_section;
+    CString m_name_section;
     /*!
-     * \brief name_comment - комментарий к секции
+     * \brief m_name_comment - комментарий к секции
      */
-    CVector<CString> name_comment;
+    CVector<CString> m_name_comment;
     /*!
-     * \brief key_value    - содержимое секции
+     * \brief m_key_value    - содержимое секции
      */
-    CMap<CString, CVector<CString>> key_value;
+    CMap<CString, CVector<CString>> m_key_value;
     void clear()
     {
-        name_section.clear();
-        name_comment.clear();
-        key_value.clear();
+        m_name_section.clear();
+        m_name_comment.clear();
+        m_key_value.clear();
     }
 };
 
@@ -76,25 +76,25 @@ public:
     void write_file(const char *file_path);
 
     /*!
-     * \brief change_name_section - поменять название секции
+     * \brief change_m_name_section - поменять название секции
      * \param old_name            - старое название
      * \param new_name            - новое название
      */
-    void change_name_section(CString &old_name, CString &new_name);
+    void change_m_name_section(CString &old_name, CString &new_name);
 
     /*!
      * \brief delete_value        - удалить полностью значение по ключу
-     * \param name_section        - секция в которой удаляем
+     * \param m_name_section        - секция в которой удаляем
      * \param key                 - ключ по которому удаляем
      */
-    void delete_value(CString &name_section, CString &key);
+    void delete_value(CString &m_name_section, CString &key);
     /*!
      * \brief delete_value        - удалить значение под номером n в CVector
-     * \param name_section        - секция в которой удаляем
+     * \param m_name_section        - секция в которой удаляем
      * \param key                 - ключ по которому удаляем
      * \param n                   - номер в CVector для удаления
      */
-    void delete_value(CString &name_section, CString &key, unsigned n);
+    void delete_value(CString &m_name_section, CString &key, unsigned n);
 
     /*!
      * \brief show_all            - демонстрация содержимого всего ini-файла
@@ -102,35 +102,35 @@ public:
     void show_all();
 
     /*!
-     * \brief change_value        - полностью меняет значение CVector, по указанному ключу
-     * \param name_section        - секция в которой меняем значение
-     * \param key                 - ключ для поиска значения
-     * \param value               - значение для замены
+     * \brief change_value          - полностью меняет значение CVector, по указанному ключу
+     * \param m_name_section        - секция в которой меняем значение
+     * \param key                   - ключ для поиска значения
+     * \param value                 - значение для замены
      */
-    void change_value(CString &name_section, CString &key, CVector<CString> &value);
+    void change_value(CString &m_name_section, CString &key, CVector<CString> &value);
     /*!
-     * \brief change_value       - меняет значение под номером n в CVector, по указанному ключу
-     * \param name_section       - секция в которой меняем значение
+     * \brief change_value         - меняет значение под номером n в CVector, по указанному ключу
+     * \param m_name_section       - секция в которой меняем значение
      * \param key                - ключ для поиска значения
      * \param value              - значение для замены
      * \param n                  - номер значения в CVector для замены
      */
-    void change_value(CString &name_section, CString &key, CString &value, unsigned n);
+    void change_value(CString &m_name_section, CString &key, CString &value, unsigned n);
 
     /*!
-     * \brief add_value           - добавляет значение которого нет в секции
-     * \param name_section        - секция в которую добавляем
+     * \brief add_value             - добавляет значение которого нет в секции
+     * \param m_name_section        - секция в которую добавляем
      * \param key                 - ключ для добавления
      * \param value               - значение для добавления
      */
-    void add_value(CString &name_section, CString &key, CVector<CString> &value);
+    void add_value(CString &m_name_section, CString &key, CVector<CString> &value);
     /*!
      * \brief add_value          - добавляет значение CString в CVector, по указанному ключу
-     * \param name_section       - секция в которую добавляем
+     * \param m_name_section       - секция в которую добавляем
      * \param key                - ключ для добавления
      * \param value              - значение для добавления
      */
-    void add_value(CString &name_section, CString &key, CString &value);
+    void add_value(CString &m_name_section, CString &key, CString &value);
 };
 
 #endif // CINI_H
